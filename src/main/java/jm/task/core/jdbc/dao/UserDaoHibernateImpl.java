5146a46d11fedaa4aa462f51e7fc02f1d7f7e93d
Pavel Kapichnikov <paval.kap@gmail.com>
2023-12-01 16:16:43 +0300

    PP_1_1_5v1

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,114 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
+import java.util.Collections;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
-    public UserDaoHibernateImpl() {
+    private final SessionFactory factory = Util.getFactory();
 
+    public UserDaoHibernateImpl() {
     }
 
 
     @Override
     public void createUsersTable() {
+        try {
+            try (Session session = factory.getCurrentSession()) {
+                String sql = """
+                        CREATE TABLE IF NOT EXISTS `users` (
+                          `id` int NOT NULL AUTO_INCREMENT,
+                          `name` varchar(45) NOT NULL,
+                          `lastName` varchar(45) NOT NULL,
+                          `age` int NOT NULL,
+                          PRIMARY KEY (`id`)
+                        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3""";
 
+                session.beginTransaction();
+                session.createNativeQuery(sql).executeUpdate();
+                session.getTransaction().commit();
+            }
+        } catch (HibernateException | NullPointerException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("БД успешно создана с помощью Hibernate");
     }
 
     @Override
     public void dropUsersTable() {
-
+        try {
+            try (Session session = factory.getCurrentSession()) {
+                session.beginTransaction();
+                session.createNativeQuery("DROP TABLE IF EXISTS users").executeUpdate();
+                session.getTransaction().commit();
+            }
+        } catch (HibernateException | NullPointerException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("БД успешно уничтожена с помощью Hibernate");
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        try {
+            try (Session session = factory.getCurrentSession()) {
+                User user = new User(name, lastName, age);
+                session.beginTransaction();
+                session.save(user);
+                session.getTransaction().commit();
+            }
+        } catch (HibernateException | NullPointerException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("User с именем – " + name + " добавлен в базу данных с помощью Hibernate");
     }
 
     @Override
     public void removeUserById(long id) {
-
+        try {
+            try (Session session = factory.getCurrentSession()) {
+                session.beginTransaction();
+                User user = session.get(User.class, id);
+                session.delete(user);
+                session.getTransaction().commit();
+            }
+        } catch (HibernateException | NullPointerException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("Пользователь с id: " + id + " успешно удалён из БД с помощью Hibernate");
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        try {
+            try (Session session = factory.getCurrentSession()) {
+                session.beginTransaction();
+                List<User> userList = session.createQuery("from User").getResultList();
+                session.getTransaction().commit();
+                return userList;
+            }
+        } catch (HibernateException | NullPointerException e) {
+            System.out.println(e.getMessage());
+            return Collections.emptyList();
+        }
     }
 
     @Override
     public void cleanUsersTable() {
-
+        try {
+            try (Session session = factory.getCurrentSession()) {
+                session.beginTransaction();
+                session.createQuery("delete User").executeUpdate();
+                session.getTransaction().commit();
+            }
+        } catch (HibernateException | NullPointerException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("Таблица успешно очищена с помощью Hibernate");
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,11 +1,17 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
     private static Connection connection;
+    private static SessionFactory factory;
     private static final String DB_URL = "jdbc:mysql://localhost:3306/db_for_pp";
     private static final String DB_USER_NAME = "pavel_kap";
     private static final String DB_PASSWORD = "Alfa-Romeo008";
@@ -35,6 +41,29 @@ public class Util {
             } catch (SQLException e) {
                 System.out.println("Не удалось закрыть соединение с БД");
             }
+        }
+    }
+
+    public static SessionFactory getFactory() {
+        try {
+            factory = new Configuration()
+                    .configure("hibernate.cfg.xml")
+                    .addAnnotatedClass(User.class)
+                    .buildSessionFactory();
+            System.out.println("Соединение с БД установлено с помощью Hibernate!");
+            return  factory;
+        } catch (HibernateException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    public static void closeFactory() {
+        try {
+            factory.close();
+            System.out.println("Factory is closed");
+        } catch (HibernateException e) {
+            System.out.println(e.getMessage());
         }
     }
 
